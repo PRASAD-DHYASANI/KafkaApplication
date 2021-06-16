3154a7aaab5061b3364620c347d92baab83c024b
dhyasani.prasadreddy@gmail.com <dhyasani.prasadreddy@gmail.com>
2021-06-16 14:46:00 -0400

    application name change

--- src/main/java/com/dhyasani/libraryeventkafkaapp/LibraryEventProducerApplication.java
+++ src/main/java/com/dhyasani/libraryeventkafkaapp/LibraryEventProducerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibraryEventKafkaAppApplication {
+public class LibraryEventProducerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LibraryEventKafkaAppApplication.class, args);
+        SpringApplication.run(LibraryEventProducerApplication.class, args);
     }
 
 }
