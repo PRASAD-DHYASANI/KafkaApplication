ce8ea9fa3b4faa2feef401bc262f90814671323d
dhyasani.prasadreddy@gmail.com <dhyasani.prasadreddy@gmail.com>
2021-06-16 14:28:58 -0400

    first commit

---
+++ src/main/java/com/dhyasani/libraryeventkafkaapp/LibraryEventKafkaAppApplication.java
@@ -0,0 +1,13 @@
+package com.dhyasani.libraryeventkafkaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryEventKafkaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryEventKafkaAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dhyasani/libraryeventkafkaapp/LibraryEventKafkaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhyasani.libraryeventkafkaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventKafkaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
